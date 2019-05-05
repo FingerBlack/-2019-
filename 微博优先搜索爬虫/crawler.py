05f8a93f710b5a32cb3e45530fae7a2e0af46c29
FingerBlack <1025982121@qq.com>
2019-05-05 12:50:31 +0800

    Update crawler.py

--- 微博优先搜索爬虫/crawler.py
+++ 微博优先搜索爬虫/crawler.py
@@ -15,14 +15,22 @@ def login():
     print("login finished")
 
 
-def getFansList(useroid):
+def getFansList(userid):
+    user_url = "https://weibo.com/u/" + str(userid)
+    wd.get(user_url)
+    fans_url = wd.find_element_by_css_selector('#Pl_Core_T8CustomTriColumn__3 > div > div > div > table > tbody > tr > td:nth-child(2) > a')
+    fans_url = fans_url.get_attribute('href')
+    print(fans_url)
+    regexp = re.compile(r"p/([0-9]*?)/")
+    useroid = regexp.findall(fans_url)
+    useroid = useroid[0]
+    print(useroid)
     final = []
     for page in range(1, 6):
         url = 'https://weibo.com/p/' + useroid + '/follow?relate=fans&page=' + str(page) + '#Pl_Official_HisRelation__59'
         wd.get(url)
         time.sleep(3)
         fansList = wd.find_elements_by_css_selector('#Pl_Official_HisRelation__59 > div > div > div > div.follow_box > div.follow_inner > ul > li')
-        print(fansList)
         regexp = re.compile(r'uid=([0-9]*?)&fnick=(.*?)&')
         for i in fansList:
             action_data = i.get_attribute('action-data')
@@ -30,7 +38,6 @@ def getFansList(useroid):
             for ele in res:
                 final.append(list(ele))
             print(action_data)
-        print(final)
     return final
 
 
@@ -40,15 +47,16 @@ if __name__ == "__main__":
     wd.set_page_load_timeout(5)
     login()
     water_army['用户3334557273'] = 1
-    usr_dict['用户3334557273'] = '1005056867108671'
-    oid_dict['1005056867108671'] = '用户3334557273'
-    cur_oid = '1005056867108671'
+    usr_dict['用户3334557273'] = '6867108671'
+    oid_dict['6867108671'] = '用户3334557273'
+    cur_oid = '6867108671'
     result = ['用户3334557273']
     cnt = int(input("Please input the number you need:"))
     cnt -= 1
     while cnt > 0:
         fans = getFansList(cur_oid)
         for i in fans:
+            if i[1] == "新手指南": continue
             if i[1] in water_army:
                 water_army[i[1]] += 1
             else:
@@ -59,9 +67,10 @@ if __name__ == "__main__":
         for i in water_army.keys():
             if (not (i in result)) and (maxUser == "" or water_army[i] > water_army[maxUser]):
                 maxUser = i
-        result.append(i)
+        result.append(maxUser)
         cur_oid = usr_dict[maxUser]
         cnt -= 1
+        print(result)
     print(result)
 
 
